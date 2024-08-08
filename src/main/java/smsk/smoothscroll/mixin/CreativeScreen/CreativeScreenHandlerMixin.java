cb766f7a1515972239dd433686629abba5e74d2e
Smajslo <16209307+SmajloSlovakian@users.noreply.github.com>
2024-08-08 11:39:41 +0200

    fixed condensed creative crash

--- src/main/java/smsk/smoothscroll/mixin/CreativeScreen/CreativeScreenHandlerMixin.java
+++ src/main/java/smsk/smoothscroll/mixin/CreativeScreen/CreativeScreenHandlerMixin.java
@@ -10,7 +10,7 @@ import net.minecraft.client.gui.screen.ingame.CreativeInventoryScreen.CreativeSc
 import smsk.smoothscroll.Config;
 import smsk.smoothscroll.SmoothSc;
 
-@Mixin(CreativeScreenHandler.class)
+@Mixin(value = CreativeScreenHandler.class, priority = 1001)
 public class CreativeScreenHandlerMixin {
 
     @ModifyVariable(method = "scrollItems", at = @At("STORE"), ordinal = 0)
@@ -24,8 +24,12 @@ public class CreativeScreenHandlerMixin {
         return (row);
     }
 
-    @Inject(method = "scrollItems", at = @At(value = "INVOKE", target = "Lnet/minecraft/inventory/SimpleInventory;setStack(ILnet/minecraft/item/ItemStack;)V"))
-    private void ItemCount(CallbackInfo ci) {
+    //@Inject(method = "scrollItems", at = @At(value = "INVOKE", target = "Lnet/minecraft/inventory/SimpleInventory;setStack(ILnet/minecraft/item/ItemStack;)V"))
+    @Inject(method = "scrollItems", at = {
+        @At(value = "INVOKE", target = "Lnet/minecraft/inventory/SimpleInventory;setStack(ILnet/minecraft/item/ItemStack;)V"),
+        @At(value = "INVOKE", target = "Lio/wispforest/condensed_creative/util/CondensedInventory;setEntryStack(ILio/wispforest/condensed_creative/entry/Entry;)V")
+    })
+    private void itemCount(CallbackInfo ci) {
         SmoothSc.creativeScreenItemCount += 1;
     }
 }
