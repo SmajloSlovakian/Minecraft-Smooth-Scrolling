130eb9665a3ffb787549641856e76ec32c1cf3bf
Smajslo <16209307+SmajloSlovakian@users.noreply.github.com>
2024-11-09 17:37:07 +0100

    repair repo

--- src/main/java/smsk/smoothscroll/SmoothSc.java
+++ src/main/java/smsk/smoothscroll/SmoothSc.java
@@ -5,7 +5,6 @@ import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.ingame.CreativeInventoryScreen.CreativeScreenHandler;
-import net.minecraft.client.render.RenderLayer;
 import net.minecraft.inventory.Inventory;
 import net.minecraft.screen.ScreenHandler;
 import net.minecraft.util.Identifier;
@@ -66,7 +65,7 @@ public class SmoothSc implements ClientModInitializer {
 		return mc.getRenderTickCounter().getLastFrameDuration();
 	}
 	public static void debugTextDraw(DrawContext context, Object s, int x, int y) {
-		context.drawText(mc.textRenderer, s + "", x, y, ColorHelper.getArgb(255, 0, 255, 255), true);
+		context.drawText(mc.textRenderer, s + "", x, y, ColorHelper.Argb.getArgb(255, 0, 255, 255), true);
 	}
 
 	public static Inventory getDelegatingInventory(ScreenHandler handler) {
@@ -82,7 +81,7 @@ public class SmoothSc implements ClientModInitializer {
     }
 	
 	public static void drawHotbarRolloverMirror(DrawContext context, Identifier texture, int x, int hotbarWidth, int offset, int y, int width, int height) {
-		context.drawGuiTexture(RenderLayer::getGuiTextured, texture, x + hotbarWidth + offset, y, width, height);
+		context.drawGuiTexture(texture, x + hotbarWidth + offset, y, width, height);
 	}
 	public static Vector3f getMatrixTranslate(DrawContext context) {
 		return context.getMatrices().peek().getPositionMatrix().getTranslation(new Vector3f(0,0,0));
