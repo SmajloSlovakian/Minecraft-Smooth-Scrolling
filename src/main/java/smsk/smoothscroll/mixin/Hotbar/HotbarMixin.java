52fac01d7379325fd144f238e6c7c44c0a229468
SmajloSlovakian <16209307+SmajloSlovakian@users.noreply.github.com>
2025-09-19 14:22:06 +0200

    Use wrapoperation and matrix translate for hotbar selector

--- src/main/java/smsk/smoothscroll/mixin/Hotbar/HotbarMixin.java
+++ src/main/java/smsk/smoothscroll/mixin/Hotbar/HotbarMixin.java
@@ -1,7 +1,11 @@
 package smsk.smoothscroll.mixin.Hotbar;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
+import com.mojang.blaze3d.pipeline.RenderPipeline;
 
+import org.joml.Vector2i;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -18,56 +22,74 @@ import net.minecraft.util.math.ColorHelper;
 import smsk.smoothscroll.SmoothSc;
 import smsk.smoothscroll.cfg.SmScCfg;
 
-@Mixin(value = InGameHud.class, priority = 999) // if bedrockify applies its mixin before smoothsc, modifyarg crashes
+/**
+ * Priority
+ * <1000: if bedrockify applies its mixin before smoothsc, modifyarg crashes
+ * <-999999999: to apply patches before raised mod (to raised mod)
+ */
+
+@Mixin(value = InGameHud.class, priority = 999)
 public class HotbarMixin {
 
-	@Unique private int rolloverOffsetR = 4; // TODO
-	@Unique private int rolloverOffsetL = 4;
-	@Unique private int rolloverOffset = 4;
-	@Unique private float selectedPixelBuffer = 0;
-	@Unique private boolean masked = false;
+	@Unique private int slotWidth = 20;
+	@Unique private int slotCount = 9;
+	@Unique private int rolloverSpace = 4;
 
-	@ModifyArgs(method = "renderHotbar", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawGuiTexture(Lcom/mojang/blaze3d/pipeline/RenderPipeline;Lnet/minecraft/util/Identifier;IIII)V", ordinal = 1))
-	private void selectedSlotX(Args args, @Local(argsOnly = true) DrawContext context) {
+	@Unique private boolean masked = false;
+	@Unique private float selectorPos = 0;
+
+	@WrapOperation(method = "renderHotbar", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawGuiTexture(Lcom/mojang/blaze3d/pipeline/RenderPipeline;Lnet/minecraft/util/Identifier;IIII)V", ordinal = 1))
+	private void moveSelector(DrawContext context, RenderPipeline pipeline, Identifier texture, int x, int y, int width, int height, Operation<Void> operation) {
 		if (SmScCfg.hotbarSmoothness == 0) return;
-		Identifier texture = args.get(1);
-		int x = args.get(2);
-		int y = args.get(3);
-		int width = args.get(4);
-		int height = args.get(5);
 		PlayerInventory inv = SmoothSc.mc.player.getInventory();
 
-		var target = (inv.getSelectedSlot() - SmoothSc.hotbarRollover * 9) * 20 - SmoothSc.hotbarRollover * rolloverOffset;
-		selectedPixelBuffer = (float) ((selectedPixelBuffer - target) * Math.pow(SmScCfg.hotbarSmoothness, SmoothSc.getLastFrameDuration()) + target);
+		var hotbarStart = x - inv.getSelectedSlot() * slotWidth;
+
+		var target = (inv.getSelectedSlot() - SmoothSc.hotbarRollover * slotCount) * slotWidth - SmoothSc.hotbarRollover * rolloverSpace;
+		selectorPos = (float) ((selectorPos - target) * Math.pow(SmScCfg.hotbarSmoothness, SmoothSc.getLastFrameDuration()) + target);
 		
-		if (Math.round(selectedPixelBuffer) < -10 - rolloverOffset) {
-			selectedPixelBuffer += 9 * 20 + rolloverOffset;
+		if (Math.round(selectorPos) <  rolloverSpace - (slotWidth / 2)) {
+			selectorPos += slotCount * slotWidth + rolloverSpace;
 			SmoothSc.hotbarRollover -= 1;
-		} else if (Math.round(selectedPixelBuffer) > 20 * 9 - 10 + rolloverOffset) {
-			selectedPixelBuffer -= 9 * 20 + rolloverOffset;
+		} else if (Math.round(selectorPos) > rolloverSpace - (slotWidth / 2) + slotWidth * slotCount) {
+			selectorPos -= slotCount * slotWidth + rolloverSpace;
 			SmoothSc.hotbarRollover += 1;
 		}
 
-		x -= inv.getSelectedSlot() * 20;
-		x += Math.round(selectedPixelBuffer);
-		args.set(2, x);
-
 		masked = false;
-		if (Math.round(selectedPixelBuffer) < 0) {
+		
+		if (Math.round(selectorPos) < 0) {
 			enableMask(context);
-			SmoothSc.drawHotbarRolloverMirror(context, texture, x, 9 * 20, rolloverOffset, y, width, height);
-		} else if (Math.round(selectedPixelBuffer) > 20 * 8) {
+			context.getMatrices().pushMatrix();
+			context.getMatrices().translate(selectorPos - (x - hotbarStart) + slotCount * slotWidth + rolloverSpace, 0);
+
+			operation.call(context, pipeline, texture, x, y, width, height);
+
+        	context.getMatrices().popMatrix();
+
+		} else if (Math.round(selectorPos) > slotWidth * 8) {
 			enableMask(context);
-			SmoothSc.drawHotbarRolloverMirror(context, texture, x, -9 * 20, -rolloverOffset, y, width, height);
+			context.getMatrices().pushMatrix();
+			context.getMatrices().translate(selectorPos - (x - hotbarStart) - slotCount * slotWidth - rolloverSpace, 0);
+
+			operation.call(context, pipeline, texture, x, y, width, height);
+
+        	context.getMatrices().popMatrix();
+		}
+
+
+        context.getMatrices().pushMatrix();
+        context.getMatrices().translate(selectorPos - (x - hotbarStart), 0);
+		operation.call(context, pipeline, texture, x, y, width, height);
+
+        context.getMatrices().popMatrix();
+		
+		if (masked) {
+			if (SmScCfg.enableMaskDebug) context.fill(-100, -100, context.getScaledWindowWidth(), context.getScaledWindowHeight(), ColorHelper.getArgb(50, 0, 255, 255));
+			context.disableScissor();
 		}
 	}
 
-	@Inject(method = "renderHotbar", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawGuiTexture(Lcom/mojang/blaze3d/pipeline/RenderPipeline;Lnet/minecraft/util/Identifier;IIII)V", ordinal = 1, shift = At.Shift.AFTER))
-	private void draw2(DrawContext context, RenderTickCounter rtc, CallbackInfo ci) {
-		if (!masked) return;
-        if (SmScCfg.enableMaskDebug) context.fill(-100, -100, context.getScaledWindowWidth(), context.getScaledWindowHeight(), ColorHelper.getArgb(50, 0, 255, 255));
-		context.disableScissor();
-	}
 
 	@Unique
 	private void enableMask(DrawContext context) {
--- src/main/java/smsk/smoothscroll/mixin/PreciseScissor/DrawContextMixin.java
+++ src/main/java/smsk/smoothscroll/mixin/PreciseScissor/DrawContextMixin.java
@@ -2,10 +2,15 @@ package smsk.smoothscroll.mixin.PreciseScissor;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import com.mojang.blaze3d.pipeline.RenderPipeline;
 
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.util.Window;
+import net.minecraft.util.Identifier;
 import smsk.smoothscroll.SmoothSc;
 
 @Mixin(DrawContext.class)
